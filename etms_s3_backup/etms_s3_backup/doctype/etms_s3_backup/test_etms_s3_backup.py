4f30e57aed3499605305f0963db35d41a998d939
itsyosefali <joeyxjoey123@gmail.com>
2023-04-16 10:36:28 +0200

    -backup app for etms sites

---
+++ etms_s3_backup/etms_s3_backup/doctype/etms_s3_backup/test_etms_s3_backup.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, yosef@ebkar.ly and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestETMSS3Backup(FrappeTestCase):
+	pass
